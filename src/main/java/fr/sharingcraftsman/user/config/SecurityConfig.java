cb91fd3ba6b01106e21c50159695ce1287602005
Cyril PHAM-LE <cyril.phamle@gmail.com>
2017-10-15 14:22:07 +0200

    Logout

--- src/main/java/fr/sharingcraftsman/user/config/SecurityConfig.java
+++ src/main/java/fr/sharingcraftsman/user/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @Configuration
 @EnableWebSecurity
@@ -18,6 +19,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers("/", "/login**", "/webjars/**")
             .permitAll()
             .anyRequest()
-            .authenticated();
+            .authenticated()
+            .and().logout().logoutSuccessUrl("/").permitAll()
+            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
   }
 }
