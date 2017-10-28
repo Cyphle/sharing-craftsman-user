987df8bde7e1e9fb3f7091a70c6395d184e3b64e
Cyril Pham-le <cyrilpham-le@macbookdecyril.home>
2017-10-28 17:05:42 +0200

    Add self hosted authorization server

--- README.md
+++ README.md
@@ -20,3 +20,23 @@ Partie autorisations: Roles gérés en interne
 Autres services feront références aux user avec leur rôle et l'id du user
 
 See Spring data rest
+
+
+Try:
+- curl acme:acmesecret@localhost:8080/oauth/token -d grant_type=client_credentials
+  -> To get client access token from self hosted authorization server
+- curl acme:acmesecret@localhost:8080/oauth/token -d grant_type=password -d username=user -d password=... (see launch logs to get the password)
+
+
+- Get github token
+- Save user infos
+- Save user token
+- Save validity date (???)
+
+- Each client will send to this app the token to get user infos
+- Each app will send with each request, its own id and secret to verify that is a sharing craftsman app
+
+- This app needs a page to register manually clients
+
+
+- Need to generate JWT token to send mailing for access ?? or just generate a random token associated with each guest
--- src/main/java/fr/sharingcraftsman/user/config/SecurityConfig.java
+++ src/main/java/fr/sharingcraftsman/user/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package fr.sharingcraftsman.user.config;
 
+import fr.sharingcraftsman.user.view.models.ClientResources;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
@@ -16,7 +17,9 @@ import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
 import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.filter.CompositeFilter;
@@ -29,32 +32,21 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 @EnableOAuth2Client
+@EnableAuthorizationServer
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   OAuth2ClientContext oauth2ClientContext;
 
   @Bean
-  @ConfigurationProperties("facebook.client")
-  public AuthorizationCodeResourceDetails facebook() {
-    return new AuthorizationCodeResourceDetails();
+  @ConfigurationProperties("github")
+  public ClientResources github() {
+    return new ClientResources();
   }
 
   @Bean
-  @ConfigurationProperties("facebook.resource")
-  public ResourceServerProperties facebookResource() {
-    return new ResourceServerProperties();
-  }
-
-  @Bean
-  @ConfigurationProperties("github.client")
-  public AuthorizationCodeResourceDetails github() {
-    return new AuthorizationCodeResourceDetails();
-  }
-
-  @Bean
-  @ConfigurationProperties("github.resource")
-  public ResourceServerProperties githubResource() {
-    return new ResourceServerProperties();
+  @ConfigurationProperties("facebook")
+  public ClientResources facebook() {
+    return new ClientResources();
   }
 
   @Bean
@@ -75,32 +67,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
             .permitAll()
             .anyRequest()
             .authenticated()
-            .and().logout().logoutSuccessUrl("/").permitAll()
-            .and().csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-            .and().addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
+            .and()
+            .exceptionHandling().authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/"))
+            .and()
+            .logout().logoutSuccessUrl("/").permitAll()
+            .and()
+            .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+            .and()
+            .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class);
   }
 
   private Filter ssoFilter() {
     CompositeFilter filter = new CompositeFilter();
     List<Filter> filters = new ArrayList<>();
-
-    OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
-    OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
-    facebookFilter.setRestTemplate(facebookTemplate);
-    UserInfoTokenServices tokenServices = new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
-    tokenServices.setRestTemplate(facebookTemplate);
-    facebookFilter.setTokenServices(tokenServices);
-    filters.add(facebookFilter);
-
-    OAuth2ClientAuthenticationProcessingFilter githubFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/github");
-    OAuth2RestTemplate githubTemplate = new OAuth2RestTemplate(github(), oauth2ClientContext);
-    githubFilter.setRestTemplate(githubTemplate);
-    tokenServices = new UserInfoTokenServices(githubResource().getUserInfoUri(), github().getClientId());
-    tokenServices.setRestTemplate(githubTemplate);
-    githubFilter.setTokenServices(tokenServices);
-    filters.add(githubFilter);
-
+    filters.add(ssoFilter(facebook(), "/login/facebook"));
+    filters.add(ssoFilter(github(), "/login/github"));
     filter.setFilters(filters);
+    return filter;
+  }
+
+  private Filter ssoFilter(ClientResources client, String path) {
+    OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
+    OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
+    filter.setRestTemplate(template);
+    UserInfoTokenServices tokenServices = new UserInfoTokenServices(client.getResource().getUserInfoUri(), client.getClient().getClientId());
+    tokenServices.setRestTemplate(template);
+    filter.setTokenServices(tokenServices);
     return filter;
   }
 }
