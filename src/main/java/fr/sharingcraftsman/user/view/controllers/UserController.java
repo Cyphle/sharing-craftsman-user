68a0c2b4adc7c0cc2185aaa70baff84904b40691
Cyril Pham-le <cyril.phamle@gmail.com>
2017-12-23 15:03:47 +0100

    Save

--- src/main/java/fr/sharingcraftsman/user/view/controllers/UserController.java
+++ src/main/java/fr/sharingcraftsman/user/view/controllers/UserController.java
@@ -5,6 +5,7 @@ import fr.sharingcraftsman.user.infrastructure.models.UserLoginType;
 import fr.sharingcraftsman.user.infrastructure.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
---
+++ src/test/java/fr/sharingcraftsman/acceptance/SpringIntegrationTest.java
@@ -0,0 +1,18 @@
+package fr.sharingcraftsman.acceptance;
+
+import fr.sharingcraftsman.user.UserApplication;
+import fr.sharingcraftsman.user.config.SecurityConfig;
+import org.springframework.boot.test.context.SpringBootContextLoader;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@ContextConfiguration(
+        classes = {UserApplication.class},
+        loader = SpringBootContextLoader.class)
+@WebAppConfiguration
+@SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
+public class SpringIntegrationTest {
+}
