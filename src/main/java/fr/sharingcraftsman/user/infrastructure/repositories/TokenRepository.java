75cb0f33529ac0919a7cb98e2639c15c5a2ba8e9
Cyril Pham-le <cyril.phamle@gmail.com>
2017-12-25 21:44:05 +0100

    token repository

--- src/main/java/fr/sharingcraftsman/user/infrastructure/repositories/TokenRepository.java
+++ src/main/java/fr/sharingcraftsman/user/infrastructure/repositories/TokenRepository.java
@@ -1,10 +1,15 @@
 package fr.sharingcraftsman.user.infrastructure.repositories;
 
 import fr.sharingcraftsman.user.infrastructure.models.OAuthToken;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import javax.transaction.Transactional;
+
 public interface TokenRepository extends CrudRepository<OAuthToken, Long> {
+  @Transactional
+  @Modifying
   @Query("Delete from OAuthToken t where t.username = ?1 and t.client = ?2")
   void deleteByUsername(String username, String client);
 }
---
+++ src/test/java/fr/sharingcraftsman/user/infrastructure/repositories/TokenRepositoryTest.java
@@ -0,0 +1,48 @@
+package fr.sharingcraftsman.user.infrastructure.repositories;
+
+import fr.sharingcraftsman.user.UserApplication;
+import fr.sharingcraftsman.user.infrastructure.models.OAuthToken;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {UserApplication.class})
+@DataJpaTest
+public class TokenRepositoryTest {
+  @Autowired
+  private TestEntityManager entityManager;
+
+  @Autowired
+  private TokenRepository tokenRepository;
+
+  @Before
+  public void setUp() throws Exception {
+    OAuthToken oAuthToken = new OAuthToken();
+    oAuthToken.setClient("client");
+    oAuthToken.setUsername("john@doe.fr");
+    oAuthToken.setAccessToken("aaa");
+    oAuthToken.setRefreshToken("bbb");
+    oAuthToken.setExpirationDate(Date.from(LocalDateTime.of(2017, 12, 25, 12, 0).atZone(ZoneId.systemDefault()).toInstant()));
+
+    entityManager.persist(oAuthToken);
+  }
+
+  @Test
+  public void should_delete_tokens_of_user() throws Exception {
+    tokenRepository.deleteByUsername("john@doe.fr", "client");
+
+    assertThat(tokenRepository.findAll()).isEmpty();
+  }
+}
