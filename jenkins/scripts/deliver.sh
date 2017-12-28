5f51039b2614a6bafc12ad85258946a323ea50c1
Cyril Pham-le <cyril.phamle@gmail.com>
2017-12-28 13:03:32 +0100

    test

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -3,6 +3,7 @@
 path='/home/apps/user'
 
 echo "Copying deployable files in app folder"
+mkdir $path
 cp docker-compose.yml $path/docker-compose.yml
 #cp Dockerfile /home/apps/user/Dockerfile
 #cp target/user-1.0.jar /home/apps/user/user.jar
