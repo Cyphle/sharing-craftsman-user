565997ef042f098e86dbf8ffae60f7a8f1044082
Cyril Pham-le <cyril.phamle@gmail.com>
2017-12-28 13:05:10 +0100

    test

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -4,7 +4,7 @@ path='/home/apps/user'
 
 echo "Copying deployable files in app folder"
 mkdir $path
-cp docker-compose.yml $path/docker-compose.yml
-#cp Dockerfile /home/apps/user/Dockerfile
+#cp docker-compose.yml $path/docker-compose.yml
+cp Dockerfile $path/Dockerfile
 #cp target/user-1.0.jar /home/apps/user/user.jar
 echo "End copying deployable files in app folder"
