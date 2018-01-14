d055d62dd9343b1aa9c06926b44ed0d11c96d09d
Cyril Pham-le <cyril.phamle@gmail.com>
2018-01-14 21:08:17 +0100

    update liquibase

--- src/main/resources/db/changelog/sql/01-create-user-table.sql
+++ src/main/resources/db/changelog/sql/01-create-user-table.sql
@@ -1,9 +1,9 @@
 CREATE TABLE users (
   id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   username varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
-  firstname varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  lastname varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   password longtext COLLATE utf8_unicode_ci NOT NULL,
+  firstname varchar(255) COLLATE utf8_unicode_ci,
+  lastname varchar(255) COLLATE utf8_unicode_ci,
   is_active tinyint(1) COLLATE utf8_unicode_ci NOT NULL,
   creation_date datetime DEFAULT NULL,
   last_update_date datetime DEFAULT NULL
