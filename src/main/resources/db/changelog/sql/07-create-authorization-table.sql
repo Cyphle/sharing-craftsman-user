cd8c8954bfb5dc0b972b5ea53d7616f18c1bfddc
Cyril Pham-le <cyril.phamle@gmail.com>
2018-01-15 20:45:33 +0100

    correct liquibase

--- src/main/resources/db/changelog/sql/07-create-authorization-table.sql
+++ src/main/resources/db/changelog/sql/07-create-authorization-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE authorizations (
   id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
-  access_group varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+  access_group varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   role varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
