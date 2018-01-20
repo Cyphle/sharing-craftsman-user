fce4a614382d623fbe66fb5cce79190f1978ca96
Cyril Pham-le <cyril.phamle@gmail.com>
2018-01-20 14:41:04 +0100

    correct scripts

--- jenkins/scripts/update-dockerfile.sh
+++ jenkins/scripts/update-dockerfile.sh
@@ -30,62 +30,80 @@ ymldata="$(parse_yaml $source)"
 
 infos=(${ymldata// / })
 
-# String to parse infos
-profileEntry='user_profile'
+# DATABASE
+
 hostEntry='user_database_host'
 userEntry='user_database_user'
 passwordEntry='user_database_password'
 portEntry='user_database_port'
 databaseNameEntry='user_database_name'
-uploadPathEntry='user_upload_path'
-logPathEntry='user_logs_path'
-appPortEntry='user_app_port'
-toInitializeEntry='user_app_toinitialize'
-dockerNetworkEntry='user_docker_network'
 
-# Length of data to get
-profileLength=$((${#infos[0]} - ${#profileEntry} - 5))
-hostLength=$((${#infos[1]} - ${#hostEntry} - 5))
-userLength=$((${#infos[2]} - ${#userEntry} - 5))
-passwordLength=$((${#infos[3]} - ${#passwordEntry} - 5))
-portLength=$((${#infos[4]} - ${#portEntry} - 5))
-databaseNameLength=$((${#infos[5]} - ${#databaseNameEntry} - 5))
-uploadPathLength=$((${#infos[6]} - ${#uploadPathEntry} - 5))
-logsPathLength=$((${#infos[7]} - ${#logPathEntry} - 5))
-appPortLength=$((${#infos[8]} - ${#appPortEntry} - 5))
-toInitializeLength=$((${#infos[9]} - ${#toInitializeEntry} - 5))
-dockerNetworkLength=$((${#infos[10]} - ${#dockerNetworkEntry} - 5))
+hostLength=$((${#infos[0]} - ${#hostEntry} - 5))
+userLength=$((${#infos[1]} - ${#userEntry} - 5))
+passwordLength=$((${#infos[2]} - ${#passwordEntry} - 5))
+portLength=$((${#infos[3]} - ${#portEntry} - 5))
+databaseNameLength=$((${#infos[4]} - ${#databaseNameEntry} - 5))
 
-# Parsed infos
-profile=${infos[0]:${#profileEntry} + 3:profileLength}
-host=${infos[1]:${#hostEntry} + 3:hostLength}
-user=${infos[2]:${#userEntry} + 3:userLength}
-password=${infos[3]:${#passwordEntry} + 3:passwordLength}
-port=${infos[4]:${#portEntry} + 3:portLength}
-database=${infos[5]:${#databaseNameEntry} + 3:databaseNameLength}
-uploadPath=${infos[6]:${#uploadPathEntry} + 3:uploadPathLength}
-logsPath=${infos[7]:${#logPathEntry} + 3:logsPathLength}
-appPort=${infos[8]:${#appPortEntry} + 3:appPortLength}
-toInitialize=${infos[9]:${#toInitializeEntry} + 3:toInitializeLength}
-dockerNetwork=${infos[10]:${#dockerNetworkEntry} + 3:dockerNetworkLength}
+host=${infos[0]:${#hostEntry} + 3:hostLength}
+user=${infos[1]:${#userEntry} + 3:userLength}
+password=${infos[2]:${#passwordEntry} + 3:passwordLength}
+port=${infos[3]:${#portEntry} + 3:portLength}
+database=${infos[4]:${#databaseNameEntry} + 3:databaseNameLength}
 
-# Replace
-sed -i -e "s/<PROFILE>/${profile}/g" $target
 sed -i -e "s/<DB_HOST>/${host}/g" $target
 sed -i -e "s/<DB_USER>/${user}/g" $target
 sed -i -e "s/<DB_PASSWORD>/${password}/g" $target
 sed -i -e "s/<DB_PORT>/${port}/g" $target
 sed -i -e "s/<DB_NAME>/${database}/g" $target
-sed -i -e 's|<UPLOAD_PATH>|'$uploadPath'|g' $target
-sed -i -e "s/<TO_INITIALIZE>/${toInitialize}/g" $target
-rm $target-e
 
-sed -i -e "s/<APP_PORT>/\"${appPort}\"/g" $composeTarget
-sed -i -e 's|<LOGS_PATH>|'$logsPath'|g' $composeTarget
+# APPLICATION
+appProfileEntry='user_app_profile'
+appUploadPathEntry='user_app_upload_path'
+appLogPathEntry='user_app_logs_path'
+appToInitializeEntry='user_app_toinitialize'
+
+appProfileLength=$((${#infos[5]} - ${#appProfileEntry} - 5))
+appUploadPathLength=$((${#infos[6]} - ${#appUploadPathEntry} - 5))
+appLogsPathLength=$((${#infos[7]} - ${#appLogPathEntry} - 5))
+appToInitializeLength=$((${#infos[8]} - ${#appToInitializeEntry} - 5))
+
+appProfile=${infos[5]:${#appProfileEntry} + 3:appProfileLength}
+appUploadPath=${infos[6]:${#appUploadPathEntry} + 3:appUploadPathLength}
+appLogsPath=${infos[7]:${#appLogPathEntry} + 3:appLogsPathLength}
+appToInitialize=${infos[8]:${#appToInitializeEntry} + 3:appToInitializeLength}
+
+sed -i -e "s/<PROFILE>/${appProfile}/g" $target
+sed -i -e 's|<UPLOAD_PATH>|'$appUploadPath'|g' $target
+sed -i -e 's|<LOGS_PATH>|'$appLogsPath'|g' $target
+sed -i -e "s/<TO_INITIALIZE>/${appToInitialize}/g" $target
+
+# DOCKER
+dockerPortEntry='user_docker_port'
+dockerUploadPathEntry='user_docker_upload_path'
+dockerLogPathEntry='user_docker_logs_path'
+dockerNetworkEntry='user_docker_network'
+
+dockerPortLength=$((${#infos[9]} - ${#dockerPortEntry} - 5))
+dockerUploadPathLength=$((${#infos[10]} - ${#dockerUploadPathEntry} - 5))
+dockerLogsPathLength=$((${#infos[11]} - ${#dockerLogPathEntry} - 5))
+dockerNetworkLength=$((${#infos[12]} - ${#dockerNetworkEntry} - 5))
+
+dockerPort=${infos[9]:${#dockerPortEntry} + 3:dockerPortLength}
+dockerUploadPath=${infos[10]:${#dockerUploadPathEntry} + 3:dockerUploadPathLength}
+dockerLogsPath=${infos[11]:${#dockerLogPathEntry} + 3:dockerLogsPathLength}
+dockerNetwork=${infos[12]:${#dockerNetworkEntry} + 3:dockerNetworkLength}
+
+sed -i -e "s/<APP_PORT>/\"${dockerPort}\"/g" $composeTarget
+sed -i -e 's|<UPLOAD_PATH>|'$dockerUploadPath'|g' $composeTarget
+sed -i -e 's|<LOGS_PATH>|'$dockerLogsPath'|g' $composeTarget
 sed -i -e "s/<DOCKER_NETWORK>/${dockerNetwork}/g" $composeTarget
-rm $composeTarget-e
 
+# LAUNCH DOCKER
 docker-compose up -d
+
+# DELETE
+rm $target-e
+rm $composeTarget-e
 rm Dockerfile
 rm docker-compose.yml
 rm user-infos.yml
