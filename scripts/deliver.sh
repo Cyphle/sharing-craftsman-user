d671c8ffb740798cdc4a5ba0f028bf227ce41c59
Cyril Pham-le <cyril.phamle@gmail.com>
2018-02-15 23:11:04 +0100

    update script

--- scripts/deliver.sh
+++ scripts/deliver.sh
@@ -6,7 +6,7 @@ mkdir $1
 cp scripts/docker-compose.yml $1/docker-compose.yml
 cp scripts/Dockerfile $1/Dockerfile
 cp target/user-1.0.jar $1/user.jar
-cp scripts/update-dockerfile.sh $1/update-dockerfile.sh
+cp scripts/update_docker_files.py $1/update_docker_files.py
 cp scripts/user-infos.yml $1/user-infos.yml
 cp scripts/README.md $1/README.md
 echo "End copying deployable files in app folder"
