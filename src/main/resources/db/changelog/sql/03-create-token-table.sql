83d38c9375703e5ea8e4978b89e8c253fb2ed389
Cyril Pham-le <cyril.phamle@gmail.com>
2018-02-16 23:44:58 +0100

    update sql

--- src/main/resources/db/changelog/sql/03-create-token-table.sql
+++ src/main/resources/db/changelog/sql/03-create-token-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE tokens (
   id int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   username varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
-  client varchar(255) COLLATE utf8_unicode_ci NOT NULL UNIQUE,
+  client varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   access_token longtext COLLATE utf8_unicode_ci NOT NULL,
   refresh_token longtext COLLATE utf8_unicode_ci NOT NULL,
   expiration_date datetime DEFAULT NULL
