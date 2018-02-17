e151b4cf359c824c356295f51426e07c0e1914d9
Cyril Pham-le <cyril.phamle@gmail.com>
2018-02-17 16:55:40 +0100

    add serializer

--- src/main/java/fr/sharingcraftsman/user/api/user/ChangePasswordTokenForLostPasswordDTO.java
+++ src/main/java/fr/sharingcraftsman/user/api/user/ChangePasswordTokenForLostPasswordDTO.java
@@ -4,12 +4,31 @@ import fr.sharingcraftsman.user.domain.common.Email;
 import fr.sharingcraftsman.user.domain.user.ChangePasswordToken;
 
 public class ChangePasswordTokenForLostPasswordDTO {
-  private final ChangePasswordTokenDTO changePasswordToken;
-  private final EmailDTO email;
+  private ChangePasswordTokenDTO changePasswordToken;
+  private EmailDTO email;
+
+  public ChangePasswordTokenForLostPasswordDTO() {
+  }
 
   private ChangePasswordTokenForLostPasswordDTO(ChangePasswordToken changePasswordToken, Email email) {
     this.changePasswordToken = ChangePasswordTokenDTO.fromDomainToApi(changePasswordToken);
     this.email = EmailDTO.fromDomainToApi(email);
+  }
+
+  public ChangePasswordTokenDTO getChangePasswordToken() {
+    return changePasswordToken;
+  }
+
+  public void setChangePasswordToken(ChangePasswordTokenDTO changePasswordToken) {
+    this.changePasswordToken = changePasswordToken;
+  }
+
+  public EmailDTO getEmail() {
+    return email;
+  }
+
+  public void setEmail(EmailDTO email) {
+    this.email = email;
   }
 
   public static ChangePasswordTokenForLostPasswordDTO from(ChangePasswordToken changePasswordToken, Email email) {
